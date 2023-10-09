683db23ab9f9c646777788ae5173634583f09be0
alice <alice@apple>
2023-10-09 12:38:26 +0800

    add flag

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"fmt"
-
+        "flag"
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
@@ -40,28 +40,44 @@ type (
 
 	// RMQ -.
 	RMQ struct {
-		ServerExchange string `env-required:"true" yaml:"rpc_server_exchange" env:"RMQ_RPC_SERVER"`
-		ClientExchange string `env-required:"true" yaml:"rpc_client_exchange" env:"RMQ_RPC_CLIENT"`
-		URL            string `env-required:"true"                            env:"RMQ_URL"`
+		ServerExchange string `env-required:"false" yaml:"rpc_server_exchange" env:"RMQ_RPC_SERVER"`
+		ClientExchange string `env-required:"false" yaml:"rpc_client_exchange" env:"RMQ_RPC_CLIENT"`
+		URL            string `env-required:"false"                            env:"RMQ_URL"`
 	}
 )
 
 // NewConfig returns app config.
 func NewConfig(cfgFile string) (*Config, error) {
-        if len(cfgFile) == 0 {
-            cfgFile = "./config/config.yml"
-        }
-
-        cfg := &Config{}
-
-	err := cleanenv.ReadConfig(cfgFile, cfg)
-	if err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
+	// config args, priority: config > consul
+	var (
+		configFile   = flag.String("config", "", "config file, prior to use.")
+		consulAddr   = flag.String("consul", "localhost:8500", "consul server address.")
+		consulFolder = flag.String("folder", "", "consul kv folder.")
+		serviceName  = flag.String("name", "microapp", "both microservice name and kv name.")
+		listenAddr   = flag.String("listen", ":8080", "listen address.")
+	)
+	flag.Parse()
+	if len(cfgFile) == 0 {
+		cfgFile = "./config/config.yml"
 	}
 
-	err = cleanenv.ReadEnv(cfg)
-	if err != nil {
-		return nil, err
+	cfg := &Config{}
+
+	if len(*configFile) == 0 {
+                fmt.Printf("consul: %s, %s.\n", *consulAddr, *consulFolder)
+		cfg.Name = *serviceName
+		cfg.Version = "v0.0.1"
+		cfg.Port = *listenAddr
+	} else {
+		err := cleanenv.ReadConfig(cfgFile, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("config error: %w", err)
+		}
+
+		err = cleanenv.ReadEnv(cfg)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return cfg, nil
